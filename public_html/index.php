eefc0ccfe903c668df03deb3aaa7bad06bc3809a
thexardas <thexardas>
2014-08-04 10:28:23 +0400

    Dictionary and frontend base table

---
+++ public_html/index.php
@@ -0,0 +1,122 @@
+<html>
+<head>
+	<style type="text/css">
+		.table
+		{
+			display: table;
+			width: auto;
+			background-color: #eee;
+			border: 1px solid #666666;
+			border-spacing: 5px;
+			/* border-collapse: separate;*/
+		}
+
+		.row
+		{
+			display: table-row;
+			width: auto;
+		}
+
+		.cell
+		{
+			float: left;
+			display: table-column;
+			width: 200px;
+			background-color: #ccc;
+		}
+	</style>
+</head>
+
+
+<body>
+	<h5>Балда</h5>
+	<div class="wrapper">
+		<div class="table">
+			<div class="row">
+				<div class="cell">
+					1
+				</div>
+				<div class="cell">
+					2
+				</div>
+				<div class="cell">
+					3
+				</div>
+				<div class="cell">
+					4
+				</div>
+				<div class="cell">
+					5
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+				<div class="cell">
+
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
